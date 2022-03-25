b25f5611d63fa9bb3e2bdbd8fe0b492ba8ba0e39
pradattapatil <pradattapatil@gmail.com>
2022-03-25 18:47:09 +0530

    Day10 Problem is added

---
+++ EmployeeWageComputation/CompanyEmpWage.java
@@ -0,0 +1,25 @@
+package Day10.EmployeeWageComputation;
+
+public class CompanyEmpWage {
+    //Declaration of Constant variables
+    public final String company;
+    public final int EMP_WAGE_PER_HOUR;
+    public final int NUM_WORKING_DAY;
+    public final int EMP_MONTHLY_WORKING_HOUR;
+    public int totalEmpWage;
+
+    public CompanyEmpWage(String company, int emp_wage_per_hour, int num_working_day, int emp_monthly_working_hour) {
+        this.company = company;
+        this.EMP_WAGE_PER_HOUR = emp_wage_per_hour;
+        this.NUM_WORKING_DAY = num_working_day;
+        this.EMP_MONTHLY_WORKING_HOUR = emp_monthly_working_hour;
+    }
+
+    public void setTotalEmpWage(int totalEmpWage){
+        this.totalEmpWage = totalEmpWage;
+    }
+    @Override
+    public String toString() {
+        return "Total emp wage for company: " + company + " is: " + totalEmpWage;
+    }
+}
